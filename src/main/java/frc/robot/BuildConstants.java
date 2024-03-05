c05861afccdf96a7507afd5e7dff8c362fa54a67
tobalerone66 <tobias.brecher1696@butlertech.org>
2024-03-05 10:57:58 -0500

    autons

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2024-Crescendo";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 94;
+  public static final String GIT_SHA = "6c2ef28688ea39c570fbb9741d8e7f7f9a252c23";
+  public static final String GIT_DATE = "2024-03-04 10:21:47 EST";
+  public static final String GIT_BRANCH = "Sonic";
+  public static final String BUILD_DATE = "2024-03-04 15:51:08 EST";
+  public static final long BUILD_UNIX_TIME = 1709585468725L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
