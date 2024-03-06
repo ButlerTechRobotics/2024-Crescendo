57f42d7f0f5aa025eb5b05b19db7e43def0b1992
tobalerone66 <tobias.brecher1696@butlertech.org>
2024-03-06 16:44:34 -0500

    .

--- src/main/java/frc/robot/subsystems/superstructure/arm/ArmPositionPID.java
+++ src/main/java/frc/robot/subsystems/superstructure/arm/ArmPositionPID.java
@@ -38,7 +38,7 @@ public class ArmPositionPID extends SubsystemBase {
     pidController.setI(kI.get(), 0);
     pidController.setD(kD.get(), 0);
     pidController.setFF(kFF.get(), 0);
-    pidController.setOutputRange(-0.40, 0.35);
+    pidController.setOutputRange(0.10, 0.1);
 
     armMotor.setIdleMode(IdleMode.kBrake);
 
