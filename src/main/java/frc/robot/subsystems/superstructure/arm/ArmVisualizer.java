a332d19226ebc5d5b9b182d913a38ae34079b237
AydenLo17 <ayden.allin3d@gmail.com>
2024-03-11 12:00:36 -0400

    Updated Sonic's Build.Gradle

--- src/main/java/frc/robot/subsystems/superstructure/arm/ArmVisualizer.java
+++ src/main/java/frc/robot/subsystems/superstructure/arm/ArmVisualizer.java
@@ -1,5 +1,5 @@
-// Copyright (c) 2024 FRC 6328
-// http://github.com/Mechanical-Advantage
+// Copyright (c) 2024 FRC 325 & 144
+// https://github.com/ButlerTechRobotics
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file at
