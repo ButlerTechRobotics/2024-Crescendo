8bda22129dd4bf4172f43fad7b8e9dc27cda3c63
tobalerone66 <tobias.brecher1696@butlertech.org>
2024-03-14 18:06:32 -0400

    auton Tests and Swerve zero

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -78,25 +78,25 @@ public final class DriveConstants {
                   1,
                   2,
                   9,
-                  Rotation2d.fromRotations(-0.372314).plus(Rotation2d.fromDegrees(180)),
+                  Rotation2d.fromRotations(-0.394043).plus(Rotation2d.fromDegrees(180)),
                   true),
               new ModuleConfig(
                   3,
                   4,
                   10,
-                  Rotation2d.fromRotations(0.181885).plus(Rotation2d.fromDegrees(0)),
+                  Rotation2d.fromRotations(0.182861).plus(Rotation2d.fromDegrees(0)),
                   true),
               new ModuleConfig(
                   5,
                   6,
                   11,
-                  Rotation2d.fromRotations(0.238770).plus(Rotation2d.fromDegrees(180)),
+                  Rotation2d.fromRotations(0.240967).plus(Rotation2d.fromDegrees(180)),
                   true),
               new ModuleConfig(
                   7,
                   8,
                   12,
-                  Rotation2d.fromRotations(-0.385498).plus(Rotation2d.fromDegrees(0)),
+                  Rotation2d.fromRotations(-0.381836).plus(Rotation2d.fromDegrees(0)),
                   true)
             };
           // .plus(Rotation2d.fromDegrees(180))
