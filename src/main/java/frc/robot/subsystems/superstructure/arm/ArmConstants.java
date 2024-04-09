a6b272ded7e1a009758e2946d0fb7d0ef0eb1aca
tobalerone66 <tobias.brecher1696@butlertech.org>
2024-04-09 18:40:41 -0400

    down speed down

---
+++ src/main/java/frc/robot/subsystems/superstructure/arm/ArmConstants.java
@@ -0,0 +1,29 @@
+// Copyright (c) 2024 FRC 325 & 144
+// https://github.com/ButlerTechRobotics
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot.subsystems.superstructure.arm;
+
+import frc.robot.Constants;
+
+public class ArmConstants {
+
+  // encoder / flywheelReduction = flywheel
+  public static double reduction = (125.0 / 1.0);
+  public static double armToleranceDegrees = 100.0;
+  public static int motorID = 22;
+
+  public static Gains gains =
+      switch (Constants.getRobot()) {
+        case SIMBOT -> new Gains(0.0, 0.0, 0.0, 0.09078, 0.00103, 0.0);
+          // case COMPBOT -> new Gains(0.00009, 0.0000002, 0.05, 8.75, 0.0027, 0.0);
+        case COMPBOT -> new Gains(0.000175, 0.0, 0.0, 0.122, 0.00175, 0.0);
+
+          // case COMPBOT -> null;
+      };
+
+  public record Gains(double kP, double kI, double kD, double kS, double kV, double kA) {}
+}
