49f71821fb3792bad6ef5c96f91323d16e265dd2
AydenLo17 <116655364+AydenLo17@users.noreply.github.com>
2024-06-15 23:25:16 -0400

    Removed unused commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,7 +14,6 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -290,16 +289,6 @@ public class RobotContainer {
     // PATHFIND TO AMP
     // ================================================
     driverController.a().whileTrue(new PathFinderAndFollow("Amp Placement Path"));
-
-    // ================================================
-    // DRIVER CONTROLLER - B
-    // PATHFIND TO STAGE SHOOT
-    // ================================================
-    driverController
-        .b()
-        .whileTrue(
-            new DriveToPoint(
-                drive, new Pose2d(new Translation2d(3.9, 5.0), Rotation2d.fromDegrees(345))));
 
     // ========================================================================================================
     // ========================================================================================================
